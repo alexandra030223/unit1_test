a6012dbcf0c02e4d870cdef7e73cb6cf458bf7a2
alexandra030223 <aleksandra2894@gmail.com>
2023-06-08 16:11:54 +0300

    -added HM for L3

--- src/Homeworks/L3/FindFac.java
+++ src/Homeworks/L3/FindFac.java
@@ -1,11 +1,13 @@
 package Homeworks.L3;
 public class FindFac {
     public static void main(String [] args){
-        for(int i = 2; i<=100; i++){
-            System.out.println(i+ " is product of: ");
+        int i;
+        for(i = 2; i<=50; i++){
+            System.out.print(i+ " is product of: ");
             for (int j = 2; j<i; j++)
-                if((i%j)==0)
+                if((i%j)==0) {
                     System.out.print(j + " ");
+                }
             System.out.println();
         }
     }
